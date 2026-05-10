2eb5ecb4a6d4f4c54e480c481adc91a5bbfda8fb
hotzzi-igmt <hotzzi5328@gmail.com>
2026-05-10 18:50:49 +0900

    Create solution_01.c
    김찬휘 문제 1

---
+++ 0506study/하지윤 문제/solution_01.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+#pragma warning(disable:4996)
+#define size = 1000
+
+//김찬휘 문제 1
+
+int main(void)
+{
+	int n;
+	int x = 0;
+
+	printf("Input a number greater than 1: ");
+	scanf("%d", &n);
+
+	if (n <= 0)
+	{
+		printf("Input error\n");
+		return 0;
+	}
+	for (int i = 1; i <= n; i++)
+	{
+		int temp = i;
+		int isValid = 1;
+
+		while (temp > 0)
+		{
+			int digit = temp % 10;
+
+			if (digit == 0 || i % digit != 0)
+			{
+				isValid = 0;
+				break;
+			}
+			temp /= 10;
+		}
+		if (isValid)
+		{
+			x = i;
+			printf("%d \n", x);
+		}
+	}
+	return 0;
+}
