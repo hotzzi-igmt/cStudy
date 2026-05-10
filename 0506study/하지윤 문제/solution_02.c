3c971ef72168ae8af823d928c17afb08a87bed3f
hotzzi-igmt <hotzzi5328@gmail.com>
2026-05-10 18:54:00 +0900

    Create solution_02.c
    김찬휘 문제 2

---
+++ 0506study/하지윤 문제/solution_02.c
@@ -0,0 +1,110 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+#pragma warning(disable:4996)
+
+//김찬휘 문제 2
+
+int countValid(int* arr, int n);
+
+int main(void)
+{
+	int n;
+	int arr[1000];
+	int count = 0;
+
+	printf("Input length of array: ");
+	scanf("%d", &n);
+
+
+	if (n < 1 || n > 1000)
+	{
+		printf("Input error");
+		return 1;
+	}
+
+	for (int i = 0;i < n;i++)
+	{
+		printf("Input value of array: ");
+		scanf("%d", &arr[i]);
+	}
+
+	int result = countValid(arr, n);
+
+	printf("count=%d\n", result);
+
+	return 0;
+}
+
+int countValid(int* arr, int n)
+{
+	int count = 0;
+
+	for (int i = 0;i < n;i++)
+	{
+		int sum = 0;
+		int max = arr[i];
+		int	min = arr[i];
+
+		for (int j = i;j < n;j++)
+		{
+			sum += arr[j];
+
+			if (arr[j] > max)
+				max = arr[j];
+
+			if (arr[j] < min)
+				min = arr[j];
+
+			int len = j - i + 1;
+
+			if (sum % len == 0)
+			{
+				int k = sum / len;
+
+				if (max - min <= k)
+				{
+					count++;
+					printf("[");
+					for (int t = i; t <= j; t++)
+					{
+						printf("%d", arr[t]);
+
+						if (t < j)
+							printf(", ");
+					}
+					printf("] K=%d\n", k);
+				}
+			}
+		}
+	}
+	return count;
+}
+
+int main(void)
+{
+	int n;
+	int arr[100];
+
+
+	scanf("%d", &n);
+	for (int i = 0; i < n;i++) {
+		scanf("%d", &arr[i]);
+	}
+	int max = arr[0];
+	int	min = arr[0];
+
+	for (int i = 0; i < n;i++) {
+		int sum = 0;
+
+
+		if (arr[i] > max)
+			max = arr[i];
+		if (arr[i] < min)
+			min = arr[i];
+	}
+	printf("%d %d\n", max, min);
+
+	return 0;
+}
